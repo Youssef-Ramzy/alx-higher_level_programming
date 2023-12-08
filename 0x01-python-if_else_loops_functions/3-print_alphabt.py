f3c49f19c5d9b41835cf1f02134c37a6999a1e8c
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-08 17:06:17 +0200

    v1

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join(["{:c}".format(i) for i in range(ord('a'), ord('z')+1) if i not in [ord('q'), ord('e')]]), end="")
+print("".join(["{:c}".format(i) for i in range(ord('\
+    a'), ord('z')+1) if i not in [ord('q'), ord('e')]]), end="")
