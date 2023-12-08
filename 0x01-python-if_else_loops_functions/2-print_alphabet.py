2dba6a6c790b8a3228efce8e08f4f26987553c5f
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-08 17:04:48 +0200

    v0

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join(["{:c}""".format(i) for i in range(ord('a'), ord('z')+1)]), end="")
+print("".join(["{:c}".format(i) \
+               for i in range(ord('a'), ord('z')+1)]), end="")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print("".join(["{:c}".format(i) for i in range(ord('a'), ord('z')+1) if i not in [ord('q'), ord('e')]]), end="")
