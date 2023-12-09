d03edcb09a59603c02ad268e01b634464ae87a9b
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-09 19:38:53 +0200

    v3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import add_0 as add
+import add_0
 
 
 def main():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add.add(a, b)))
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
 
 
 if __name__ == "__main__":
