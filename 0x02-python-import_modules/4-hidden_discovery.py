0ded9ff6b5bafe58ace30a738f1d06da0aaddcb9
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-10 04:41:08 +0200

    v0

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
-
-
-def main():
-    
-
-
-if __name__ == "___main__":
-    main()
+if __name__ == "__main__":
+    import importlib
+    import inspect
+    if __name__ == "__main__":
+        hidden_4 = importlib.import_module('hidden_4')
+        names = dir(hidden_4)
+        names = [name for name in names if not name.startswith('__')]
+        names.sort()
+        for name in names:
+            print(name)
