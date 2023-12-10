6933ab424add039f37ab50c6686d5cacb2e77c9f
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-10 04:31:30 +0200

    v3

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-from variable_load_5 import a
+if __name__ == "__main__":
+    from variable_load_5 import a
 
 
-def main():
     print(a)
-
-
-if __name__ == "___main__":
-    main()
