398c18dc76a8e5cb1e1c6b454fa00aedcc02752a
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-17 00:40:39 +0200

    v0

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx <= len(my_list) and idx >= 0:
+        while int != idx:
+            int + 1
+        my_list[idx] = element
+        return my_list
+    return None
