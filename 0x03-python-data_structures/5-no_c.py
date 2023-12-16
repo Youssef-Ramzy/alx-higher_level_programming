e8c11112877b00ad0e3efb6feb219e8980aec9a3
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-17 01:29:31 +0200

    v0

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace('c', '').replace('C', '')
-    return new_string
+	new_string = ''.join([char for char in my_string if char not in 'cC'])
+	return new_string
