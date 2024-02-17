2b263ca0f08ee8ccac35637553a5bb2a23951f14
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-02-17 16:19:44 +0200

    v0

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+    """Square class with size"""
     def __init__(self, size=0):
         """Initializes the data."""
         self.__size = size
@@ -14,3 +15,25 @@ class Square:
     def area(self):
         """Returns the current square area"""
         return self.__size ** 2
+
+    @property
+    def size(self):
+        """Retrieve the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
