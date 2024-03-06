ece5884ee3f42cb0167bedc84cb6ff3573c0953d
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-06 07:37:08 +0200

    v1

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,6 +4,8 @@
 
 class Rectangle:
     """ class rectangel """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
