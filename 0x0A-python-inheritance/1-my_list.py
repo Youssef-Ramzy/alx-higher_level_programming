49db56f51ca277b02469c6205c4bf116e8b767d8
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-15 16:50:57 +0200

    v1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module for lookup method"""
+""" Module that contains a class MyList that inherits from list """
 
 
 class Mylist(list):
     """ class that inherate from list """
     def print_sorted(self):
-        """ method that print lists """
+        """ method that print lists in sorted """
         print(sorted(self))
