e70e8f23e1af7a6a3be72d8801648aa30b312fa6
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-15 17:01:24 +0200

    v0

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module for the 2-is_same_class.py """
+
+
+def is_same_class(obj, a_class):
+    """ method check if the obj is in the class """
+    if obj in a_class:
+        return True
+    else:
+        return False
