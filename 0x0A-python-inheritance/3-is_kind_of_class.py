2d1dc68a159d88a6ea0d00ec41c380110fd5e519
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-15 17:08:23 +0200

    v0

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module of 3-is_kind_of_class.py """
+
+
+def is_kind_of_class(obj, a_class):
+    """ method to check if the obj is from class """
+
+    return isinstance(obj, a_class)
