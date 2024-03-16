833e63c129ba890890e71ed8243b0821dbcbec05
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-16 11:25:33 +0200

    v0

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3
-""" Write a class Rectangle that inherits from BaseGeometry. """
+""" Module for defining a Square class which inherits from Rectangle. """
 Rectangle = __import__('9-rectangle.py').Rectangle
 
 
 class Square(Rectangle):
-    """ a class for the task """
-    def __init__(self, size):
-        """ instantiation with size """
-        self.integer_validator("size", size)
-        self.__size = size
+	""" 
+	Square class that inherits from Rectangle.
+	
+	Attributes:
+		__size (int): size of the square.
+	"""
+	
+	def __init__(self, size):
+		""" 
+		Initialize Square instance with size. 
+		
+		Args:
+			size (int): size of the square.
+		"""
+		self.integer_validator("size", size)
+		self.__size = size
 
-    def area(self):
-        """ returns the area of the square """
-        return (self.__size * self.__size)
+	def area(self):
+		""" 
+		Calculate and return the area of the square. 
+		
+		Returns:
+			int: The area of the square.
+		"""
+		return (self.__size * self.__size)
 
-    def __str__(self):
-        """ returns the string representation of the square """
-        return ("[Square] {}/{}".format(self.__size, self.__size))
+	def __str__(self):
+		""" 
+		Define the print() representation of a Square instance. 
+		
+		Returns:
+			str: A string representation of the square.
+		"""
+		return ("[Square] {}/{}".format(self.__size, self.__size))
