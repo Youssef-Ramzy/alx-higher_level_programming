324a5f51478b2c8e00a92632324dabe6cc368979
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-23 16:32:28 +0200

    v0

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Append to a file"""
 
 
-def append_write(filename="", text=""):
+def append_write(filename="UTF8.txt", text=""):
     """Append to a file"""
     with open(filename, 'a') as file:
         return file.write(text)
