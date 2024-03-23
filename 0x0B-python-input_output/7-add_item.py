00ee0db3cba3825351ccbbaa7e5b6db7a24524f8
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-23 17:01:31 +0200

    V1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,15 +5,18 @@ load_from_json_file = \
         __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
+def main():
+    """Main function"""
+    args = sys.argv[1:]
+    filename = "add_item.json"
 
-args = sys.argv[1:]
-filename = "add_item.json"
+    try:
+        items = load_from_json_file(filename)
+    except FileNotFoundError:
+        items = []
 
-try:
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    items = []
+    items.extend(args)
+    save_to_json_file(items, filename)
 
-items.extend(args)
-save_to_json_file(items, filename)
-print("Items added successfully!")
+if __name__ == "__main__":
+    main()
