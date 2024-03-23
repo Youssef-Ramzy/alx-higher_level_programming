dabce1116648558b791bc29fd572633a09089e60
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-23 16:40:36 +0200

    v0

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Load from JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Load from JSON file"""
+    with open(filename, "r") as file:
+        return json.load(file)
