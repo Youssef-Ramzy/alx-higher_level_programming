eee1b428f79e36816a791236a606320137dbf35c
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-23 16:30:59 +0200

    v3

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """Write file"""
 
 
-def write_file(filename="UTF8", text=""):
-    '''Write file'''
-    with open(filename + ".txt", "w") as file:
+def write_file(filename="UTF8.txt", text=""):
+    """Write file"""
+    with open(filename, 'w') as file:
         return file.write(text)
