f8a595cf56f7e3fd4fcc74dc79b898ebf74286be
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-29 15:32:45 +0200

    v0

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nb_print = 0
+    for i in range(x):
+        try:
+            if isinstance(my_list, int):
+                print("{:d}".format(my_list[i]), end="")
+                nb_print += 1
+        except IndexError:
+            break
+    print()
+    return nb_print
