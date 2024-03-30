c22b0f90e20feaa227059863e290db170510dbf3
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-30 22:27:51 +0200

    v5

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,8 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return c
     finally:
-        print("Inside result: {:.1f}".format(c))
+        if c is None:
+            print("Inside result: None")
+        else:
+            print("Inside result: {:.1f}".format(c))
         return c
