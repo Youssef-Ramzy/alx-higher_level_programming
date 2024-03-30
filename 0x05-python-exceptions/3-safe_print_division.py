34331b297043d520bc903452ae45c35edea11104
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-30 22:26:13 +0200

    v4

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         c = a / b
     except ZeroDivisionError:
-        pass
+        return c
     finally:
         print("Inside result: {:.1f}".format(c))
         return c
