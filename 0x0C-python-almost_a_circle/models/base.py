6872d47226f6c43e3b19305fd4dfb9c42e382fbc
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-03-31 23:21:45 +0200

    v0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,11 @@
+class Base:
+    """Base class for all other classes in the project"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes the base class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
