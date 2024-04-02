4fb454485dcbdacb99e79f0e49ac3ea82be2aed9
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-04-02 22:12:06 +0200

    v0

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This is a module for the square project. """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This class represents a square."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+            x (int): The x-coordinate of the square's position.
+            y (int): The y-coordinate of the square's position.
+            id (int): The id of the square.
+
+        """
+        super().__init__(id, x, y, size)
+        self.size = size
+
+    def __str__(self):
+        """Return a string representation of the square.
+
+        Returns:
+            str: A string representation of the square.
+
+        """
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.size))
