eb50f49ab1d4b3e6bdbb8d41198a360eabfddde7
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-09 15:04:58 +0000

    v2

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let new_list = [];
-  for (let i = list.length -1; i >= 0; i--) {
+  const new_list = [];
+  for (let i = list.length - 1; i >= 0; i--) {
     new_list.push(list[i]);
   }
   return new_list;
