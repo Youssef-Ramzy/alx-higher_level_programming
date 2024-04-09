be89dd9fd7e64e54541035b1bc9859f309d29acb
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-09 14:54:35 +0000

    v1

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let num of list) {
+  for (const num of list) {
     if (num === searchElement) {
       count++;
     }
