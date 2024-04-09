1637c0d8c689a0192c9b830c7af129d7b950de58
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-09 02:48:33 +0000

    fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,8 +6,8 @@ function add (a, b) {
 
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
-if (isNaN(arg1) || isNaN(arg2)) {
-  console.log('Missing size');
+if (!arg1 || !arg2) {
+  console.log('NaN');
 } else {
   add(arg1, arg2);
 }
