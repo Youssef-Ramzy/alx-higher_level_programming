92c634a6d6e258a20f8157e7fd89e1ca9f815e60
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-09 13:19:53 +0000

    v0

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      // empty
-    } else if (!w || !h) {
-      // empty
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
