1644996e84bd81fa7a4bcde19ce0aae431c94e14
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-09 02:36:22 +0000

    fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const arg = process.argv[2];
 const size = +arg;
 
 if (isNaN(size)) { console.log('Missing size'); } else {
-  for (let i = 0; i <= size; i++) {
+  for (let i = 0; i < size; i++) {
     let row = '';
-    for (let j = 0; j <= size; j++) { row += 'X'; }
+    for (let j = 0; j < size; j++) { row += 'X'; }
     console.log(row);
   }
 }
