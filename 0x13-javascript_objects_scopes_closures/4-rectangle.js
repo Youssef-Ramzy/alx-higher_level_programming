bef23350ff2c3e75a379f00ab50270612e5c95a1
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-09 13:17:51 +0000

    v3

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      // empty
-    } else if (!w || !h) {
-      // empty
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
@@ -13,11 +9,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += 'x';
-      }
-      console.log(row);
+      console.log('X'.repeat(this.width));
     }
   }
 }
