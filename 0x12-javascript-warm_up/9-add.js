207cae8c67d098e3a6e4d6c61814d692710a6036
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-09 02:38:57 +0000

    fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 function add (a, b) {
-  const numA = Number(a);
-  const numB = Number(b);
-  if (isNaN(numA) || isNaN(numB)) {
-    console.log('Missing size');
-    return;
-  }
+  const numA = parseInt(a);
+  const numB = parseInt(b);
   const result = numA + numB;
   console.log(result);
 }
 
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
-
+if (isNaN(arg1) || isNaN(arg2)) {
+  console.log('Missing size');
+  return;
+}
 add(arg1, arg2);
