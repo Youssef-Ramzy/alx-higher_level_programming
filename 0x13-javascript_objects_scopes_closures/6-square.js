6c07bf3ce19bac34c58417176abeb3073554c64a
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-09 14:19:39 +0000

    v7

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,13 +7,7 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.width; i++) {
-        console.log(c.repeat(this.width));
-      }
-    }
+    this.print(c);
   }
 }
 
