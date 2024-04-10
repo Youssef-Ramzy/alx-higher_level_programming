fbddf715194bb09b98989fd0deb2cc30742a2d4f
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-10 02:11:54 +0000

    v0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -52,3 +52,18 @@ class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 f.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string.
+
+        Args:
+            json_string (str): A JSON string.
+
+        Returns:
+            obj: The deserialized object.
+        """
+        import json
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
