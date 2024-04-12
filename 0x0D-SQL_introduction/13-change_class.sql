44e727300c57211920165ae88704830deb134268
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-12 20:11:15 +0000

    v2

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,3 @@
--- create change in the 13-change_class.sql
-DELETE FROM 'second_table' WHERE 'score' <= 5;
+--  a script that removes all records with a score <= 5 in the table second_table
+DELETE FROM `second_table`
+WHERE `score` <= 5;
