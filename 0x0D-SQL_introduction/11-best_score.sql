859d651b55911dfa32303f89dc8307b63d3001bc
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-12 20:00:19 +0000

    v2

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- 11-best_score.sql
-SELECT 'score' , 'naem' FROM 'second_table' WHERE 'score' >= 10 ORDER BY 'score' DESC;
+SELECT 'score' , 'name' FROM 'second_table' WHERE 'score' >= 10 ORDER BY 'score' DESC;
