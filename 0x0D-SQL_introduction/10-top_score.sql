1c918530a2eaf02125dbc406390ed6bce8bd51e0
Youssef Ramzy <yousseframzyvd@gmail.com>
2024-04-12 19:55:01 +0000

    v3

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
--- 10-top_score.sql
-SELECT 'score' , 'name' FROM 'second_table' ORDER BY 'score' DESC;
+-- a script that lists all records of the table second_table of the database
+SELECT `score` , `name` FROM `second_table`  ORDER BY `score` DESC;
