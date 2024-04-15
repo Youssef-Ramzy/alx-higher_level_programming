be76990fbea3c028ce37588b6dc38b2bc8e8de6e
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-04-15 20:02:51 +0200

    v3

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
--- write a script that create a database and a table
+-- Check if the database exists, if not, create it
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREAET TABLE IF NOT EXISTS states (
-	id INT AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
