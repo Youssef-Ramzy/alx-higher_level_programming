43ec0a9df761404c8986ef687c10e2fb4fcfe96d
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-04-15 19:58:30 +0200

    v0

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- write a script that create a database and a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREAET TABLE IF NOT EXISTS states (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
