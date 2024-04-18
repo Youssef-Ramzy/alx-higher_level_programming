e2435696afc92ddf6f99fdb604f37df08f824867
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-04-18 03:27:27 +0200

    v5

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,8 @@ def main():
     )
     cur = con.cursor()
     name = sys.argv[4]
-    query = "SELECT * FROM states WHERE name LIKE %s;"
-    cur.execute(query, (name,))
+    query = "SELECT * FROM states WHERE name LIKE '{}';".format(name)
+    cur.execute(query)
     result = cur.fetchall()
     for row in result:
         print(row)
