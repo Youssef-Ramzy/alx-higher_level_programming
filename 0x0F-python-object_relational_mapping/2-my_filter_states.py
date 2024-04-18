9657047618ba75d71981199aab7834e2f31d96bc
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-04-18 03:16:00 +0200

    v0

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This script is to list all the states from hbtn_0e_0_usa database """
+import MySQLdb
+import sys
+
+
+def main():
+    """ main function """
+    con = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = con.cursor()
+    name = sys.argv[4]
+    compire = "SELECT * FROM states WHERE name LIKE %s;"
+    cur.executemany(compire, name)
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    con.commit()
+    cur.close()
+    con.close()
+
+
+if __name__ == "__main__":
+    main()
