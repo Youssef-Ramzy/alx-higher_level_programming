096f117839a666e6f0931512e643951aa946dbfc
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-04-18 05:08:42 +0200

    v0

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This script is to list all the states from hbtn_0e_0_usa database """
+import MySQLdb
+import sys
+
+
+def main():
+    """ main function """
+    con = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = con.cursor()
+    name_of_state = sys.argv[4]
+    quary = """SELECT c.name
+                FROM cities AS c, states AS s
+                WHERE c.state_id = s.id
+                ORDER BY c.id ASC;
+                """
+    cur.execute(quary, (name_of_state,))
+    result = cur.fetchall()
+    for row in result:
+        if (row+1 != None):
+            print(row, end=", ")
+    con.commit()
+    cur.close()
+
+
+if __name__ == "__main__":
+    main()
